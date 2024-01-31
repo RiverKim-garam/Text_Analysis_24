c64adf7422df6672305823b3a13c3ef1dbd0d302
RiverKim(Garam) <145672185+RiverKim-garam@users.noreply.github.com>
2024-01-31 00:32:15 +0000

    Create Exer_1.R

---
+++ Exer_1.R
@@ -0,0 +1,8 @@
+install.packages("ggthemes")
+install.packages("babynames")
+
+library(tidyverse)
+library(tidytext)
+library(ggthemes) # includes a set of themes for visualizations
+library(readr) # more informative and easy way to import data
+library(babynames) #for gender predictions
